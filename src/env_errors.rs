a4cab4c08fd0264a6830198d65de4c671fec1fb2
Émile Boucher <emile.boucher@hotmail.fr>
2023-02-12 13:43:04 -0500

    update documentation

--- src/env_errors.rs
+++ src/env_errors.rs
@@ -1,3 +1,5 @@
+/// # Environment Enum Result
+/// Anything that isn't in the Ok status is an error.
 #[derive(PartialEq, Debug)]
 pub enum EnvEnumResult<T: Clone> {
     Ok(T),
@@ -6,6 +8,12 @@ pub enum EnvEnumResult<T: Clone> {
 }
 
 impl<T: Clone> EnvEnumResult<T> {
+    /// Will panic for everything that isn't Ok
+    /// ```
+    /// use dotenv_enum::env_errors::EnvEnumResult;
+    ///
+    /// assert_eq!(EnvEnumResult::Ok(4).panic_if_absent(), 4)
+    /// ```
     pub fn panic_if_absent(&self) -> T {
         match self {
             EnvEnumResult::Ok(val) => val.clone(),
